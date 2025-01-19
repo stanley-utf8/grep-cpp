e26206775828d9bfc57f2f4743756e2386657dd0
twinfantasyfan <wangstanley910@gmail.com>
2025-01-19 16:20:38 -0500

    codecrafters submit [skip ci]

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,6 +1,9 @@
+#include <cctype>
 #include <iostream>
 #include <regex>
 #include <string>
+#include <unordered_map>
+#include <unordered_set>
 
 bool match_pattern(const std::string &input_line, const std::string &pattern) {
   if (pattern.length() == 1) {
@@ -8,14 +11,62 @@ bool match_pattern(const std::string &input_line, const std::string &pattern) {
   }
 
   if (pattern == "\\d") {
-    std::regex digit_pattern("[0-9]");
-    // std::regex digit_pattern("\\d");
-    return std::regex_search(input_line, digit_pattern);
+    // regex version
+    // std::regex digit_pattern("[0-9]");
+    // return std::regex_search(input_line, digit_pattern);
+    for (char c : input_line) {
+      if (isdigit(c)) {
+        return true;
+      }
+    }
+    return false;
   }
 
   if (pattern == "\\w") {
-    std::regex word_pattern("[a-z|A-Z|0-9|_]");
-    return std::regex_search(input_line, word_pattern);
+    // regex version
+    // std::regex word_pattern("[a-z|A-Z|0-9|_]");
+    // return std::regex_search(input_line, word_pattern);
+    for (char c : input_line) {
+      if (isalnum(c) || c == '_') {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  if (pattern[0] == '[') {
+    int idx = 1;
+    std::unordered_set<char> char_set;
+    while (idx + 1 < pattern.length() - 1 || pattern[idx + 1] == ']') {
+      char cur = pattern[idx];
+      char next = pattern[idx + 1];
+
+      if (next == '-') {
+        if (idx + 2 < pattern.length() - 1) {
+          char_set.insert(cur);
+          char_set.insert(next);
+          idx += 2;
+        } else {
+          int start = cur - '0';
+          int end = pattern[idx + 2] - '0';
+          for (char c = start; c <= end; c++) {
+            char_set.insert(c);
+          }
+          idx += 3;
+        }
+      } else {
+        char_set.insert(cur);
+        idx += 1;
+      }
+    }
+
+    for (char c : input_line) {
+      if (char_set.find(c) != char_set.end()) {
+        return true;
+      }
+    }
+    return false;
+
   }
 
   else {
